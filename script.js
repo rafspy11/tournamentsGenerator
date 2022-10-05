dd117dbef2ee7812824df6d6c98f6df7738c4df1
Rafal Spychaj <rafspy11@gmail.com>
2022-10-05 13:56:30 +0200

    shuffle games

--- script.js
+++ script.js
@@ -1,59 +1,100 @@
 
 function generateTournament() {
-  let teams = [
+  let teamsArr = [
     {
-      id: 0,
+      id: 1,
       name: "Polska",
       points: 0,
       games: 0,
     },
     {
-      id: 1,
+      id: 2,
       name: "Senegal",
       points: 0,
       games: 0,
     },
     {
-      id: 2,
+      id: 3,
       name: "Kolumbia",
       points: 0,
       games: 0,
     },
     {
-      id: 3,
+      id: 4,
       name: "Japonia",
       points: 0,
       games: 0,
     },
   ];
 
-  let numberOfRounds = teams.length - 1;
-
-  function generateRounds() {
-    let tournamentDiv = document.getElementById("tournament");
+  function generateRounds(teams) {
+    let numberOfRounds = teams.length - 1;
+    let scheduleArray = [];
 
     for (i = 0; i < numberOfRounds; i++) {
-      let roundDiv = document.createElement("div");
-      roundDiv.innerHTML = "Kolejka " + (i + 1);
-      roundDiv.classList.add("round");
+      let roundArray = [];
 
       for (let j = 0; j < teams.length / 2; j++) {
-        let gameDiv = document.createElement("div");
-        gameDiv.innerHTML =
-          teams[j].name + " - " + teams[teams.length - 1 - j].name;
-        gameDiv.classList.add("game");
-        gameDiv.dataset.team1Id = teams[j].id;
-        gameDiv.dataset.team2Id = teams[teams.length - 1 - j].id;
-        gameDiv.dataset.team1Score = 1;
-        gameDiv.dataset.team2Score = 0;
-
-        roundDiv.appendChild(gameDiv);
+        roundArray.push([teams[j].id, teams[teams.length - 1 - j].id]);
       }
 
       teams.splice(1, 0, teams[teams.length - 1]);
       teams.pop();
 
-      tournamentDiv.appendChild(roundDiv);
+      scheduleArray.push(roundArray);
+    }
+
+    return scheduleArray;
+  }
+
+  function shuffleGames(scheduleArr) {
+
+    // losowa kolejnosc gospodarz - gosc
+    for(let i = 0; i < scheduleArr.length; i++) {
+        for(let j = 0; j < scheduleArr[i].length; j++) {
+            shuffleArray(scheduleArr[i][j]);
+        }
+    }
+
+    // losowe mecze w kolejce
+    for(let i = 0; i < scheduleArr.length; i++) {
+        shuffleArray(scheduleArr[i]);
+    }
+
+    // losowe kolejki
+    shuffleArray(scheduleArr);
+
+    return scheduleArr;
+  }
+
+  function generateRoundsDisplay() {
+    let scheduleArr = generateRounds(teamsArr);
+    scheduleArr = shuffleGames(scheduleArr);
+    let tournamentDiv = document.getElementById("tournament");
+    
+    for(let i = 0; i < scheduleArr.length; i++) {
+        let roundDiv = document.createElement("div");
+        roundDiv.innerHTML = "Kolejka " + (i + 1);
+        roundDiv.classList.add("round");
+
+        for(let j = 0; j < scheduleArr[i].length; j++) {
+            let teams = scheduleArr[i][j];
+            let team1 = teamsArr.find((team) => team.id == teams[0]);
+            let team2 = teamsArr.find((team) => team.id == teams[1]);
+            let gameDiv = document.createElement("div");
+            gameDiv.innerHTML = team1.name + " - " + team2.name;
+            gameDiv.classList.add("game");
+            gameDiv.dataset.team1Id = team1.id;
+            gameDiv.dataset.team2Id = team2.id;
+            gameDiv.dataset.team1Score = 1;
+            gameDiv.dataset.team2Score = 0;
+
+            roundDiv.appendChild(gameDiv);
+
+            console.log(scheduleArr[i][j]);
+        }
+
+        tournamentDiv.appendChild(roundDiv);
     }
   }
 
@@ -63,8 +104,8 @@ function generateTournament() {
     for (let i = 0; i < games.length; i++) {
       let score1 = parseInt(games[i].dataset.team1Score);
       let score2 = parseInt(games[i].dataset.team2Score);
-      let team1 = teams.find((team) => team.id == games[i].dataset.team1Id);
-      let team2 = teams.find((team) => team.id == games[i].dataset.team2Id);
+      let team1 = teamsArr.find((team) => team.id == games[i].dataset.team1Id);
+      let team2 = teamsArr.find((team) => team.id == games[i].dataset.team2Id);
       if (score1 && score2 >= 0) {
         if (score1 > score2) {
           team1.points += 3;
@@ -78,21 +119,6 @@ function generateTournament() {
         team2.games++;
       }
     }
-    console.log(teams);
-  }
-
-  function shuffle() {
-    let container = document.getElementById("tournament");
-    let elementsArray = Array.prototype.slice.call(
-      container.getElementsByClassName("round")
-    );
-    elementsArray.forEach(function (element) {
-      container.removeChild(element);
-    });
-    shuffleArray(elementsArray);
-    elementsArray.forEach(function (element) {
-      container.appendChild(element);
-    });
   }
 
   function shuffleArray(array) {
@@ -105,12 +131,11 @@ function generateTournament() {
     return array;
   }
 
-  generateRounds();
+  generateRoundsDisplay();
   calculateScores();
-  shuffle();
 }
 
 setTimeout(function () {
   generateTournament();
-}, 3000);
+}, 1000);
 
